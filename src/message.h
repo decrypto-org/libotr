3566bfe112a8988e94cb7bb12208000d8427fe44
mandragore <gkonstandinos@gmail.com>
2016-07-18 00:01:37 +0300

    Add code to pass UI/UX information needed by an application

--- src/message.h
+++ src/message.h
@@ -298,6 +298,7 @@ typedef struct s_OtrlMessageAppOps {
     /* DIKOMAS */
     char **(*chat_get_participants)(void *opdata, const char *accountname, const char *protocol, otrl_chat_token_t chat_token, unsigned int *size);
     void (*chat_privkey_create)(void *opdata, const char *accountname, const char *protocol);
+    void (*chat_info_refresh)(void *opdata, const OtrlChatInfo *info);
     /***********/
 
 } OtrlMessageAppOps;
